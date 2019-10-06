606169de0dada4bb048a61250ac0c419e0b39db3
kimseak-dev <kimseakphon@gmail.com>
2019-10-06 14:52:30 +0700

    spring-jdbc

---
+++ src/main/java/com/seak/jdbc/dao/IAccountDAO.java
@@ -0,0 +1,17 @@
+package com.seak.jdbc.dao;
+
+import com.seak.jdbc.entity.Account;
+
+import java.util.List;
+
+public interface IAccountDAO {
+    int add(Account account);
+
+    int update(Account account);
+
+    int delete(int id);
+
+    Account findAccountById(int id);
+
+    List<Account> findAccountList();
+}
---
+++ src/main/java/com/seak/jdbc/service/IAccountService.java
@@ -0,0 +1,18 @@
+package com.seak.jdbc.service;
+
+import com.seak.jdbc.entity.Account;
+
+import java.util.List;
+
+public interface IAccountService {
+
+    int add(Account account);
+
+    int update(Account account);
+
+    int delete(int id);
+
+    Account findAccountById(int id);
+
+    List<Account> findAccountList();
+}
